a830387ce2eb055815240e6a6f419b4d1226f089
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2024-03-13 18:57:03 -0300

    Add files via upload

---
+++ Gerador de Senhas/gerador_de_senhas.js
@@ -0,0 +1,18 @@
+let form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    let senha = "";
+    let maiuscula = form.letra_maiuscula;
+    let minuscula = form.letra_minuscula;
+    let numero = form.numero;
+    let simbolo = form.simbolo;
+
+    if (maiuscula.value) {
+        let letra = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        document.querySelector('#senha').value = letra; 
+        letra = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        document.querySelector('#senha').value += letra; 
+    }
+})
